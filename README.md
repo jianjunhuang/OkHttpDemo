4285013194cae8791cba4c38cd1f7a90bcbf7010
JianjunHuang <903732218@qq.com>
2017-02-26 22:13:22 +0800

    修改成 utf-8 ，修改 README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # OkHttpDemo
 OkHttp demo include OkHttp utils library
+## 依赖
+```
+compile 'com.library.jianjunhuang.okhttputils:okhttputils:0.0.2'
+```
+## 代码
+```
+private void getAsy(String url) {
+        OkHttpUtils.getInstance().getAsy()
+                .baseURL(url)
+                .build()
+                .execute(new ResultCallback() {
+                    @Override
+                    public void onError(Call call, IOException e) {
+                        Toast.makeText(SampleActivity.this, "err", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(Response response) {
+                        Toast.makeText(SampleActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void postAsy(String url) {
+        OkHttpUtils.getInstance().postAsy()
+                .baseURL(url)
+                .params("size", "10")
+                .build()
+                .execute(new ResultCallback() {
+                    @Override
+                    public void onError(Call call, IOException e) {
+                        Toast.makeText(SampleActivity.this, "err", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(Response response) {
+                        Toast.makeText(SampleActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+```
--- okhttputils/src/main/java/com/library/jianjunhuang/okhttputils/okhttputils/request/PostRequest.java
+++ okhttputils/src/main/java/com/library/jianjunhuang/okhttputils/okhttputils/request/PostRequest.java
@@ -2,6 +2,7 @@ package com.library.jianjunhuang.okhttputils.okhttputils.request;
 
 import java.util.Map;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 
@@ -10,26 +11,30 @@ import okhttp3.RequestBody;
  * @since 2017/2/8.
  */
 
-public class PostRequest extends OkHttpRequest<PostRequest>{
+public class PostRequest extends OkHttpRequest<PostRequest> {
 
+  public PostRequest(String url, Map<String, String> params, Map<String, String> headers) {
+    super(url, params, headers);
+  }
 
-    public PostRequest(String url, Map<String, String> params,Map<String,String> headers) {
-        super(url, params,headers);
+  @Override protected RequestBody buildRequestBody() {
+    RequestBody requestBody;
+    FormBody.Builder builder = new FormBody.Builder();
+    StringBuffer sb = new StringBuffer();
+
+    for (Map.Entry<String, String> entry : params.entrySet()) {
+      //builder.add(entry.getKey(),entry.getValue());
+      sb.append(entry.getKey() + "=" + entry.getValue() + "&");
     }
 
-    @Override
-    protected RequestBody buildRequestBody() {
-        RequestBody requestBody ;
-        FormBody.Builder builder = new FormBody.Builder();
-        for(Map.Entry<String,String> entry : params.entrySet()){
-            builder.add(entry.getKey(),entry.getValue());
-        }
-        requestBody = builder.build();
-        return requestBody;
-    }
+    //requestBody = builder.build();
+    requestBody =
+        RequestBody.create(MediaType.parse("application/x-www-form-urlencoded; charset=utf-8"),
+            sb.toString());
+    return requestBody;
+  }
 
-    @Override
-    protected Request buildRequest(RequestBody requestBody,Request.Builder builder) {
-        return builder.post(requestBody).build();
-    }
+  @Override protected Request buildRequest(RequestBody requestBody, Request.Builder builder) {
+    return builder.post(requestBody).build();
+  }
 }
